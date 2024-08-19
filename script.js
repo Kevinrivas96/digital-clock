03ef0a5166c8368a379928cb6a89e11f5d35c751
Kevinrivas96 <99252792+Kevinrivas96@users.noreply.github.com>
2024-08-18 20:47:51 -0400

    Refactored code.

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-// retrieve all the elements from the DOM
+// Retrieve all the elements from the DOM
 const $hours = document.querySelector('#hours');
 const $minutes = document.querySelector('#minutes');
 const $seconds = document.querySelector('#seconds');
 const $currentYear = document.querySelector('#currentYear');
-const $sound = document.querySelector('#soundIcon>i');
+const $sound = document.querySelector('#soundIcon > i');
 const $video = document.querySelector('video');
 
-// update the time every second
-setInterval(() => {
-    const now = new Date();
-    let hours = now.getHours();
-    let minutes = now.getMinutes().toString().padStart(2, '0');
-    let seconds = now.getSeconds().toString().padStart(2, '0');
+// Update the time every second
+setInterval(updateTime, 1000);
 
-    hours = hours > 12 ? hours - 12 : hours;
-    hours = hours.toString().padStart(2, '0');
+// Update the time
+function updateTime() {
+    const now = new Date();
+    const hours = String(now.getHours() % 12 || 12).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
 
     $hours.textContent = hours;
     $minutes.textContent = minutes;
     $seconds.textContent = seconds;
-}, 1000);
+}
 
-// get the current year
+// Get the current year
 $currentYear.textContent = new Date().getFullYear();
 
-// toggle the sound icon
+// Toggle the sound icon
 $sound.addEventListener('click', () => {
     $video.muted = !$video.muted;
-    if ($video.muted) {
-        $sound.classList.remove('fa-volume-xmark');
-        $sound.classList.add('fa-volume-high');
-    } else {
-        $sound.classList.remove('fa-volume-high');
-        $sound.classList.add('fa-volume-xmark');
-    }
+    $sound.classList.toggle('fa-volume-xmark', $video.muted);
+    $sound.classList.toggle('fa-volume-high', !$video.muted);
 });
+
+// update the time on page load
+updateTime();
